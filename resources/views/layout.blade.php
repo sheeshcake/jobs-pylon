97fd55067907f632efc8667dde6c7a43f4e2e3ce
wendale1231 <spooderm5@gmail.com>
2021-05-17 16:15:50 +0800

    changes footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -53,11 +53,11 @@
               <img src="{{ url('/') }}/img/logo.png" alt="">
     
             </a>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+            <p>Like and Follow us on:</p>
             <div class="social-links mt-3">
               <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bi bi-instagram bx bxl-instagram"></i></a>
+              <a href="https://www.facebook.com/PylonOnline" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="https://www.instagram.com/pylononline/" class="instagram"><i class="bi bi-instagram bx bxl-instagram"></i></a>
               <a href="#" class="linkedin"><i class="bi bi-linkedin bx bxl-linkedin"></i></a>
             </div>
           </div>
@@ -68,30 +68,26 @@
               <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
               <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
               <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
             </ul>
           </div>
 
           <div class="col-lg-2 col-6 footer-links">
             <h4>Our Services</h4>
             <ul>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Social Media Marketing</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Branding Strategy</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Website Development</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Contact Center</a></li>
             </ul>
           </div>
 
           <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
             <h4>Contact Us</h4>
             <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+            Benito Labao Ext. Roosevelt,
+            Brgy. Saray-Tibanga 9200 Iligan City <br><br>
+              <strong>Tel No.:</strong> (063) 228-4272 or (063) 302 9758<br>
+              <strong>Email:</strong> pylonglobal@gmail.com<br>
             </p>
 
           </div>
@@ -102,7 +98,7 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>FlexStart</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Pylon Jobs</span></strong>. All Rights Reserved
       </div>
       
     </div>
